3034c8b2fdf8f33027e8225ffdb5aa501165667d
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2013-10-27 10:39:09 -0400

    Added caching and ability to pass in path to AngularJS source.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-var jsdom = require('jsdom').jsdom;
+var jsdom = require('jsdom').jsdom,
+	ngPath;
 
 // create global vars angular assumes are available
 global.document = jsdom('<html><head></head><body></body></html>');
 global.window = document.parentWindow;
 global.navigator = {};
 
-// read angular source into memory
-var src = require('fs').readFileSync(require.resolve('./lib/angular.js'), 'utf8');
-// replace implicit reference
-src = src.replace('angular.element(document)', 'window.angular.element(document)');
-// run it!
-eval(src);
+function compile() {
+	// read angular source into memory
+	var src = require('fs').readFileSync(ngPath, 'utf8');
+	// replace implicit reference
+	src = src.replace('angular.element(document)', 'window.angular.element(document)');
+	// run it!
+	eval(src);
+}
 
 // share the love
-module.exports = global.window.angular;
+module.exports = function (path, overwrite) {
+	if (global.window.angular && !overwrite) return global.window.angular;
+
+	ngPath = path ? require('path').resolve(process.cwd(), path) : require.resolve('./lib/angular.js');
+	compile();
+
+	return global.window.angular;
+}
