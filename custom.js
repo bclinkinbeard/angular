b1245cc3e622165dbb0f868f816614a020f2d7d4
Tom Yam <tomyam1@gmail.com>
2014-04-24 13:12:53 +0300

    require angularjs instead of creating a Function to enable debugging

--- custom.js
+++ custom.js
@@ -3,15 +3,38 @@ var fs = require('fs'),
 	window = document.parentWindow;
 
 module.exports = function (path) {
-	// read angular source into memory
-	var src = fs.readFileSync(path, 'utf8');
 
-	// replace implicit references
-	src = src.split('angular.element(document)').join('window.angular.element(document)');
-	src = src.split('(navigator.userAgent)').join('(window.navigator.userAgent)');
-	src = src.split('angular.$$csp').join('window.angular.$$csp');
+  var prevAngular, prevDocument, prevNavigator, prevWindow;
 
-	(new Function('window', 'document', src))(window, document);
+  // save global state before
+  prevWindow = global.window;
+  prevDocument = global.document;
+  prevNavigator = global.navigator;
+  prevAngular = global.angular;
+
+  // this enables us to `require` angular's code
+  global.window = window;
+  global.document = document;
+  global.navigator = window.navigator;
+
+  // needed because angular is referenced as angular instead of window.angular
+  global.angular = {
+    $$csp: function() {
+      return window.angular.$$csp.apply(window.angular, arguments);
+    },
+    element: function() {
+      return window.angular.element.apply(window.angular, arguments);
+    }
+  };
+
+  // require angular's code
+  require(path);
+
+  // restore global state
+  global.window = prevWindow;
+  global.document = prevDocument;
+  global.navigator = prevNavigator;
+  global.angular = prevAngular;
 
 	return window.angular;
-}
+};
